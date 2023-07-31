145264e4d6e746e2a1273a6e78a3a4ce8cc96f41
Franklin Candanoza <fcandanoza@Franklins-MacBook-Pro.local>
2023-07-30 21:29:09 -0500

    remove unrequired routes

--- backend/app.py
+++ backend/app.py
@@ -42,33 +42,3 @@ app.add_middleware(
 )
 
 app.include_router(contact.router)
-        
-"""
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
-"""
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/contacts/{contact_id}")
-async def get_contact(contact_id: int):
-    return {"message": contact_id}
-
-@app.get("/contacts-model/{model_name}")
-async def get_contact_type(model_name: ModelName):
-    return {"message": model_name}
-
-@app.get("/elements/")
-async def get_contact_type(nombre: str, cedula: Optional[int] = None):
-    return {"nombre": nombre, "cedula": cedula}
-
-@app.post("/contacts/")
-async def create_contact(contact: Contact):
-    return {"nombre": contact.name}
